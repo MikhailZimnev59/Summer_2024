2b956e1d776fbb7677f294e0f8fbf86d44d2094a
Mikhail_Z <m.zimnev@yandex.ru>
2024-07-11 09:54:34 +0300

    Lesson 28

--- Task_1_1.py
+++ Task_1_1.py
@@ -1,1 +1,3 @@
 print('Task_1_1')
+def fu_print():
+    print('fu_print from Task_1_1')
--- main.py
+++ main.py
@@ -1,16 +1,17 @@
-# This is a sample Python script.
+import pytest, unittest
+def should_skip():
+    return True
+class Test(unittest.TestCase):
+    def a(self):
+        self.assertEqual(1+1, 2)
+    def test_b(self):
+        self.assertEqual(1+2, 3)
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+@pytest.mark.skip
+def test_c(self):
+    self.assertEqual(1+3, 4)
+@pytest.mark.skipif('should_skip')
+def test_d(self):
+    self.assertEqual(1+4, 5)
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
